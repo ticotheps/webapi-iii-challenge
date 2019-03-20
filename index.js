beeb95c8449c0cb1a4405a9133e07318fab3c527
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-20 16:13:13 -0400

    Creates a route for a GET request at '/' and creates routing for '/api/users'

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./server.js');
 
 server.listen(6000, () => {
-    console.log("Welcome to Tico's Web-API-III-Challenge Server, running on http://localhost:6000");
+    console.log('Server is running on http://localhost:6000');
 });
--- server.js
+++ server.js
@@ -9,4 +9,14 @@ const server = express();
 server.use(express.json()); // <== this is a built-in middleware
 server.use(helmet()); // <== this is a third party middleware, requires 'yarn add'
 
+// routing
+server.use('/api/users', usersRouter);
+
+server.get('/', (req, res) => {
+    res.send(`
+    <h1>Welcome to Tico's Web-API-III-Challenge Server</h1>
+    <h3>This server is running on http://localhost:6000"</h3>
+    `);
+});
+
 module.exports = server;

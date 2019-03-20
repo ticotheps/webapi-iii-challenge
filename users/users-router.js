1383f8822f65dbbe4e99e1e4f1b02d7ca8ba69db
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-20 17:09:22 -0400

    Creates a 'router.get()' route for asynchronous action to retrieve 'users' from the database

--- users/users-router.js
+++ users/users-router.js
@@ -3,3 +3,17 @@ const express = require('express');
 const Users = require('./users-model.js');
 
 const router = express.Router();
+
+// this only runs if the url has /api/users in it
+router.get('/', async (req, res) => {
+    try {
+        const users = await Users.find(req.query); 
+        res.status(200).json(users);
+    } catch (error) {
+        // logs error to database
+        console.log(error);
+        res.status(500).json({
+            message: 'Error retrieving the users',
+        });
+    }
+});

c80efd5b33445ff995db5cecd78314aea2204007
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-20 16:28:26 -0400

    Creates find() function inside of users-model.js file

--- users/users-model.js
+++ users/users-model.js
@@ -13,3 +13,25 @@ module.exports = {
     addPost,
 };
 
+function find(query) {
+    let { page = 1, limit = 5, sortby = 'id', sortdir = 'asc' } = query;
+    // limit = Math.min(limit, 10);
+    const offset = limit * (page - 1);
+
+    // const response = {
+    //     results: data,
+    //     pagination: {
+    //         page: 2,
+    //         limit: 100,
+    //         count: 45,
+    //         totalRecords: 145,
+    //     },
+    // };
+
+    let rows = db('users')
+        .orderBy(sortby, sortdir)
+        .limit(limit)
+        .offset(offset);
+
+    return rows;
+}

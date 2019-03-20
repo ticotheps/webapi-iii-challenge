70b2e251d6c608e47b75ab3e24650528d3da2757
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-20 16:24:31 -0400

    Defines 'db' inside of users-model.js and exports the following (from users-model.js): find, findById, add, remove, update, findUserPosts, findPostsById, addPost

--- users/users-model.js
+++ users/users-model.js
@@ -1,4 +1,15 @@
 const knex = require('knex');
 const knexConfig = require('../knexfile.js');
+const db = knex(config.development);
 
-module.exports = knex(knexConfig.development);
+module.exports = {
+    find,
+    findById,
+    add,
+    remove,
+    update,
+    findUserPosts,
+    findPostsById,
+    addPost,
+};
+

0f78f5aa68e8679668286c483c21c9475061d6bf
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-21 16:11:58 -0400

    Creates an asynchronous endpoint for '/' in server.js

--- server.js
+++ server.js
@@ -21,11 +21,20 @@ server.use(ensureUpperCaseName);
 // routing
 server.use('/api/users', usersRouter);
 
-server.get('/', (req, res) => {
-    res.send(`
-    <h1>Welcome to Tico's Web-API-III-Challenge Server</h1>
-    <h3>This server is running on http://localhost:6000"</h3>
-    `);
-});
+// server.get('/', (req, res) => {
+//     res.send(`
+//     <h1>Welcome to Tico's Web-API-III-Challenge Server</h1>
+//     <h3>This server is running on http://localhost:6000"</h3>
+//     `);
+// });
+
+server.get('/', async (req, res) => {
+    try {
+      res.status(200).json({ greeting: `${process.env.GREETING}`});
+    } catch (error) {
+      console.error('\nERROR', error);
+      res.status(500).json({ error: 'Sorry, not enough swag to gain access to this server.' });
+    }
+  });
 
 module.exports = server;

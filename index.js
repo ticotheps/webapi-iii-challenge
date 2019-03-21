937dbdc4503473ac2006463ed89a16f5ae23ad3a
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-21 16:04:11 -0400

    Defines 'port' dynamically inside of index.js file and uses interpolation in listener

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const server = require('./server.js');
 
+const port = process.env.PORT || 4000;
+
+const greeting = process.env.GREETING;
+
 server.listen(6000, () => {
-    console.log('\n* Server is running on http://localhost:6000 *\n');
+    console.log(`\n* ${greeting}! Server is running on http://localhost:${port} *\n`);
 });
